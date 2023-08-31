ef2ae0a01a201c83f90036a9b74fc2428fd2aa5c
Fickry Iman <fickry.iman@gmail.com>
2023-08-31 20:51:46 +0700

    linters fixes #2

--- class/person.rb
+++ class/person.rb
@@ -2,8 +2,8 @@ require_relative 'nameable'
 require_relative 'rental'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
-  attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
--- class/rental.rb
+++ class/rental.rb
@@ -4,7 +4,8 @@ require_relative 'person'
 require 'date'
 
 class Rental
-  attr_accessor :date, :book, :person
+  attr_accessor :date
+  attr_reader :book, :person
 
   def initialize(date, book, person)
     @date = date
