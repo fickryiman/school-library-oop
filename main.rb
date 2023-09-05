34a812be678de2e5625d66e6cb51cbb8150b70e2
Yusuf Obr <yousarsif80@gmail.com>
2023-09-05 14:41:36 +0100

    clean up the runner

--- main.rb
+++ main.rb
@@ -1,10 +1,17 @@
 require_relative 'app'
+def start
+  app = App.new
+  puts "Welcome to School Library App! \n\n"
+  loop do
+    menu_display
+    option = gets.chomp.to_i
+    puts 'Thank you for using this app!' if option == 7
+    options(app, option)
+  end
+end
 
-app = App.new
-
-puts "Welcome to School Library App! \n\n"
-
-loop do
+def menu_display
+  puts "\n"
   puts 'Choose an option:'
   puts '1. List all books'
   puts '2. List all people'
@@ -13,25 +20,23 @@ loop do
   puts '5. Create a rental'
   puts '6. List all rentals for a given person ID'
   puts '7. Quit'
-  option = gets.chomp
-
-  case option
-  when '1'
-    app.list_books
-  when '2'
-    app.list_persons
-  when '3'
-    app.create_person
-  when '4'
-    app.create_book
-  when '5'
-    app.create_rental
-  when '6'
-    app.list_rentals_by_person_id
-  when '7'
-    print 'Thank you for using this app!'
-    break
-  end
-
-  puts "\n"
 end
+
+def options(app, choice)
+  option = {
+    1 => :list_books,
+    2 => :list_persons,
+    3 => :create_person,
+    4 => :create_book,
+    5 => :create_rental,
+    6 => :list_rentals_by_person_id,
+    7 => :exit
+  }
+  action = option[choice]
+  if action
+    app.send(action)
+  else
+    puts 'Invalid input! Please try again.'
+  end
+end
+start
